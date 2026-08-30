7987bd9eb63ae6a93680f71e6b74b091e690c073
Neil <neil@logan-5.co.uk>
2026-08-30 13:19:42 +0100

    modified goat counter event text

--- src/main.js
+++ src/main.js
@@ -4235,7 +4235,7 @@ function checkAnswer() {
   if (studentGates <= state.challenge.minimalGateCount) {
     state.solved = true;
     recordSolvedChallenge(ast);
-    trackGoatcounterEvent("Boolinator - Question Completed", "/question-completed");
+    trackGoatcounterEvent("Boolinator - Question Completed", "/boolinator-question-completed");
     setFeedback("Equivalent and minimal. You solved this challenge.", "success", []);
     hintArea.classList.remove("hidden");
     renderHint();
